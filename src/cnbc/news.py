34889a968817878e6447db333d1f2a35372470b5
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-13 16:35:50 -0400

    feat: add list_special_reports to news.py

--- src/cnbc/news.py
+++ src/cnbc/news.py
@@ -23,3 +23,28 @@ def list_trending(count: str, api_key: str):
     response = requests.request("GET", url, headers=headers, params=querystring).json()
 
     return response
+
+
+def list_special_reports(api_key: str, page_size: str = 25, page: str = 1):
+    """
+    List special reports.
+
+    :param api_key: An API key to CNBC API.
+    :param page_size: For paging purposes.
+    :param page: For paging purposes.
+
+    :return: API response in JSON.
+    """
+    url = "https://cnbc.p.rapidapi.com/news/v2/list-special-reports"
+    querystring = {
+        "pageSize": page_size,
+        "page": page
+    }
+    headers = {
+        'x-rapidapi-host': "cnbc.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+    return response
