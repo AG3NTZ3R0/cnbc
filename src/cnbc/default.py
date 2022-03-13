4a080865dcc2d41693fdc9eed3e24ae47dca906a
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-13 15:54:04 -0400

    feat: add auto_complete to default.py

--- src/cnbc/default.py
+++ src/cnbc/default.py
@@ -19,3 +19,25 @@ def get_meta_data(api_key: str):
 
     return response
 
+
+def auto_complete(query: str, api_key: str):
+    """
+    Get auto complete suggestions by term or phrase.
+
+    :param query: Any familiar term or phrase to get auto complete suggestions.
+    :param api_key: An API key from CNBC API.
+
+    :return: API response in JSON.
+    """
+    url = "https://cnbc.p.rapidapi.com/v2/auto-complete"
+    querystring = {
+        "q": query
+    }
+    headers = {
+        'x-rapidapi-host': "cnbc.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+    return response
