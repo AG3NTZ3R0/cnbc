51999674d3b4737bc894455cf4676583b204a526
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-13 17:17:14 -0400

    test: test_market.py

---
+++ tests/test_market.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+
+
+from src.cnbc.market import *
+
+
+class TestMarketEndpoints(unittest.TestCase):
+    def test_list_indices(self):
+        """
+        Test that get_metadata is communicating with the CNBC API.
+        """
+        json_resp = list_indices(os.environ.get('RAPID_API_KEY'))
+        self.assertEqual(type(json_resp), dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
