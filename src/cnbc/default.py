457a0aa2f5118e1cdf08c2c48921d12f023d5b4d
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-13 15:44:28 -0400

    feat: add get_meta_data to default.py

---
+++ src/cnbc/default.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def get_meta_data(api_key: str):
+    """
+    Get metadata that supports other endpoints.
+
+    :param api_key: An API key from CNBC API.
+
+    :return: API response in JSON.
+    """
+    url = "https://cnbc.p.rapidapi.com/get-meta-data"
+    headers = {
+        'x-rapidapi-host': "cnbc.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+    }
+    response = requests.request("GET", url, headers=headers).json()
+
+    return response
+
