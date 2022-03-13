cb7ee98656479a94de7e7031bd625b4411323676
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-13 16:18:58 -0400

    feat: add get_peers to symbol.py

--- src/cnbc/symbol.py
+++ src/cnbc/symbol.py
@@ -166,3 +166,26 @@ def get_priceline_chart(issue_id: str, num_of_days: str, api_key: str):
     response = requests.request("GET", url, headers=headers, params=querystring).json()
 
     return response
+
+
+def get_peers(symbol: str, api_key: str):
+    """
+    Get peers relating to stock quote, index, exchange, etc.
+
+    :param symbol: Symbol of a stock, index, exchange, etc.
+    :param api_key: An API key to CNBC API.
+
+    :return: API response in JSON.
+    """
+    url = "https://cnbc.p.rapidapi.com/symbols/get-peers"
+    querystring = {
+        "symbol": symbol
+    }
+    headers = {
+        'x-rapidapi-host': "cnbc.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+    return response
