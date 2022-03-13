8f6a55d10b29c042d1d596064cb90b64ba5e535c
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-13 16:06:18 -0400

    feat: add get_profile and get_chart to symbol.py

--- src/cnbc/symbol.py
+++ src/cnbc/symbol.py
@@ -25,3 +25,50 @@ def get_earnings_chart(issue_id: str, number_of_years: str, api_key: str):
 
     return response
 
+
+def get_profile(issue_id: str, api_key: str):
+    """
+    Get summary information of stock quote, index, exchange, etc.
+
+    :param issue_id: The value of the issueId field returned in auto-complete or translate endpoints.
+    :param api_key: An API key from CNBC API.
+
+    :return: API response in JSON.
+    """
+    url = "https://cnbc.p.rapidapi.com/symbols/get-profile"
+    querystring = {
+        "issueId": "36276"
+    }
+    headers = {
+        'x-rapidapi-host': "cnbc.p.rapidapi.com",
+        'x-rapidapi-key': "d73bb60f82mshbe3e55c57b941abp1abe67jsn7d7492f26dee"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+    return response
+
+
+def get_chart(issue_id: str, interval: str, api_key: str):
+    """
+    Get raw data to draw price chart of stock quote, index, exchange, etc.
+
+    :param issue_id: The value of the issueId field returned in auto-complete or translate endpoints.
+    :param interval: One of the following: 1d, 5d, 1m, 3m, 6m, ytd, 1y, 3y, 5y, 10y, all.
+    :param api_key: An API key from CNBC API.
+
+    :return: API response in JSON.
+    """
+    url = "https://cnbc.p.rapidapi.com/symbols/get-chart"
+    querystring = {
+        "symbol": issue_id,
+        "interval": interval
+    }
+    headers = {
+        'x-rapidapi-host': "cnbc.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+    return response
